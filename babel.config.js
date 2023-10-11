3f1bc05064c9f9bfbbfee7883dff275da1320a28
ShawnS2020 <shawnathon4@gmail.com>
2023-10-10 20:11:39 -0400

    uninstalled some unnecessary babel plugins

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    // plugins: ['transform-decorators-legacy',
-      // 'transform-class-properties',]
+    presets: ['babel-preset-expo']
   };
 };
