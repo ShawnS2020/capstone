85501e8b19da3ddc3f69d9314bfd61c4b996c42d
ShawnS2020 <shawnathon4@gmail.com>
2023-10-12 01:13:04 -0400

    Changed Stack Nav to Tab Nav

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider } from 'mobx-react';
 import counterScreenStore from './state/CounterScreenStore.js';
 import dummyAccountStore from './state/DummyAccountStore.js';
-import HomeScreen from './screens/HomeScreen.js';]
+import HomeScreen from './screens/HomeScreen.js';
 import ActivityScreen from './screens/ActivityScreen.js';
 import LocationScreen from './screens/LocationScreen.js';
 import CounterScreen from './screens/CounterScreen.js';
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <Provider counterScreenStore={counterScreenStore} dummyAccountStore={dummyAccountStore}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            initialParams={{ counterScreenStore, dummyAccountStore }}
+        <Tab.Navigator>
+          <Tab.Screen 
+            name="Activity Screen" 
+            component={ActivityScreen}
+            initialParams={{ dummyAccountStore }}
           />
-          <Stack.Screen
-            name="Counter"
-            component={CounterScreen}
-            initialParams={{ counterScreenStore }}
-          />
-          <Stack.Screen
+          <Tab.Screen
             name="Location"
             component={LocationScreen}
           />
-          <Stack.Screen 
-            name="ActivityScreen" 
-            component={ActivityScreen}
-          />
-        </Stack.Navigator>
+        </Tab.Navigator>
         <StatusBar style="auto" />
       </NavigationContainer>
     </Provider>
