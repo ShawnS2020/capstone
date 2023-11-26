388c43c74106696c11b9f32042a4d6eee658a638
thomas szczesniak <tszczesniak7@gmail.com>
2023-11-26 17:19:30 -0500

    have to login before accessing forums

--- src/screens/ForumHubScreen.js
+++ src/screens/ForumHubScreen.js
@@ -2,23 +2,84 @@ import { StyleSheet, View, Text, TextInput, Button, FlatList } from 'react-nativ
 
 export default function ForumHubScreen({ navigation }) {
     return(
-        <View>
-            <Button
-                onPress={() => navigation.navigate('Forum')}
-                title="Forum 1"
-            />
-            <Button
-                onPress={() => navigation.navigate('Forum')}
-                title="Forum 2"
-            />
-            <Button
-                onPress={() => navigation.navigate('Login')}
-                title="Login/Register"
-            />
+        <View style={ styles.container }>
+            <Text style = {styles.headerText}>
+                        Login to Access Forums
+                    </Text>
+            <View>
+                <Button
+                    onPress={() => navigation.navigate('Login')}
+                    title="Login/Register"
+                />
+            </View>
         </View>
     );
 }
 
-const styes = StyleSheet.create({
-
-})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+        
+    inputContainer: {
+        width: '60%',
+    
+    },
+    headerContainer: {
+        flexDirection: 'row', 
+        alignItems: 'center', 
+    },
+    input: {
+        backgroundColor: 'rgba(183, 13, 1, .5)',
+        color: 'white',
+        fontWeight: '100',
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+        borderRadius: 5,
+        marginTop: 10,
+    
+    },
+    buttonContainer: {
+        width: '40%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+    
+    },
+    button: {
+        width: '100%',
+        height: 50,
+        alignItems: 'center',
+        backgroundColor: 'rgba(183, 13, 1, .7)',
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+        borderRadius: 5,
+        marginTop: 5,
+    
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '900',
+    
+    },
+    buttonOutline: {
+        backgroundColor: 'transparent'
+        
+    
+    },
+    buttonOutlineText: {
+        color: 'white',
+        fontWeight: '900',
+    
+    
+    },
+    headerText:{
+        fontSize: 50,
+        color: 'rgba(183, 13, 1, .7)',
+        textAlign:'center',
+        fontWeight: '100',
+    }
+    })
